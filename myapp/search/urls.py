8058268593739cd8b391a26fcb615ec9a83a444d
j.ikegami <cz510009@gmail.com>
2021-12-29 18:38:49 +0900

    urls.py修正

--- myapp/search/urls.py
+++ myapp/search/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login, logout
 urlpatterns = [
     path('result', views.result, name='result'),
     path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('favorite/', views.favorite, name='favorite'),
     url(r'^post/', views.for_ajax),
     path('favorite/delete/', views.delete),
